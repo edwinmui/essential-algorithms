bb539f12f394399033551e138a3a819d1e5a9e1a
edwinmui <edwinmui@umich.edu>
2020-06-01 16:38:55 -0700

    feat: Add Binary Search

--- binary_search.h
+++ binary_search.h
@@ -12,26 +12,33 @@
 #include "helpers.h"
 #include <vector>
 
-// REQUIRES: nums is a valid vectors of ints, left and right refer to the left-
-//           most and right-most indexes of nums, respectively
+// REQUIRES: nums is a valid, sorted vectors of ints, left and right refer to
+//           the left-most and right-most indexes of nums, respectively
 // EFFECTS:  If target is found in the vector nums, returns index of that
 //           target; othewise returns -1
 int binary_search(std::vector<int> nums, int target, int left, int right){
     // gets the midpoint index b/e left and right
-    int mid = (left + right) / 2;
-    // base case; if target is foun
+    int mid = 1 + (right-1)/2;
+    // base case; if target is found
     if(nums[mid] == target){
         return mid;
     }
+    // base case; if target is not found
+    if(left > right){
+        return -1;
+    }
+    // edge case; if target is the first value of the vector
+    if(nums[left] == target){
+        return left;
+    }
+    
     // if middle is less than target
-    else if(nums[mid] < target){
-        return binary_search(nums, target, mid, right);
+    if(nums[mid] < target){
+        return binary_search(nums, target, mid+1, right);
     // if middle is larger than target
     }else{
         return binary_search(nums, target, left, mid);
     }
-    // returns -1 if target is not found
-    return -1;
 }
 
 #endif /* binary_search_h */
